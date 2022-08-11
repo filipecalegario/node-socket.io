eaad877ae5f1e41a8840fc47424c20256cd22e00
filipecalegario <fcac@cin.ufpe.br>
2022-08-10 23:55:49 -0300

    exploratory coding

--- server.js
+++ server.js
@@ -5,16 +5,39 @@ const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
+const ADMIN = '/admin.html';
 
-const server = express()
+const app = express()
+
+const server = app
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+const orderNamespace = io.of("/admin");
+
+orderNamespace.on("connection", (socket) => {
+  socket.join("room1");
+  orderNamespace.to("room1").emit("hello");
+  console.log(`id ${socket.id} entered as order`)
 });
 
-setInterval(() => io.emit('time', {date: new Date().toTimeString(), text: "texto"}), 1000);
+const userNamespace = io.of("/users");
+
+userNamespace.on("connection", (socket) => {
+  socket.join("room1"); // distinct from the room in the "orders" namespace
+  userNamespace.to("room1").emit("holà");
+});
+
+io.on('connection',  (socket) => {
+  console.log("Clients count " + io.engine.clientsCount);
+  console.log('Client connected ' + socket.id);
+  socket.on('disconnect', () => console.log('Client disconnected ' + socket.id));
+  socket.on('print', () => {
+    console.log(`socket ${socket.id} emitted print`);
+  })
+});
+
+setInterval(() => io.emit('time', { date: new Date().toTimeString(), text: "texto", count: io.engine.clientsCount }), 1000);
+
